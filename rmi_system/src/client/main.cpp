4f6162ff23cd5c6d5b77bafe41091c972dd7529e
watzum <emil.watz@gmail.com>
2022-03-01 11:15:19 +0100

    Füge CLI11-Bibliothek hinzu

--- rmi_system/src/client/main.cpp
+++ rmi_system/src/client/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <asio.hpp>
+#include "CLI11.hpp"
 
 using namespace std;
 using namespace asio;
 
-int main() {
+int main(int argc, char* argv[]) {
+    CLI::App app{"RMI Server"};
+    CLI11_PARSE(app, argc, argv);
     std::cout << "Hello, I am the client!" << std::endl;
     auto a1{ip::make_address("127.0.0.1")};
     cout << a1.to_string() << ", " << a1 << endl;   
--- rmi_system/src/server/main.cpp
+++ rmi_system/src/server/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <asio.hpp>
+#include "CLI11.hpp"
+
 
 using namespace std;
 using namespace asio;
 
-int main() {
+int main(int argc, char* argv[]) {
+    CLI::App app{"RMI Server"};
+    CLI11_PARSE(app, argc, argv);
     std::cout << "Hello, I am the server!" << std::endl;
     auto a1{ip::make_address("127.0.0.1")};
     cout << a1.to_string() << ", " << a1 << endl;   
